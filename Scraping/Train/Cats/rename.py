cbfee355030c82ce3476824dae2ac8e99080641e
Shreya Anand <106630585+Anand-shreya@users.noreply.github.com>
2024-06-15 11:46:23 +0530

    Delete Scraping/Train/Cats/rename.py

--- Scraping/Train/Cats/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for i in range(1, 1136):
-    try:
-        old_file = os.path.join("Cats/Real_cats", "%s.jpg"%i)
-        new_file = os.path.join("Cats/AI_gen_cats", "Real_%s.jpg"%i)
-        os.rename(old_file, new_file)
-    except:
-        print("file doesnot exist %s"%i)
